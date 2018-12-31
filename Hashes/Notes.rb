7d409f0b065df37b7dcca771620435fe8db5db24
Jose <joegonzo990@gmail.com>
2018-12-31 14:28:34 +0000

    Hash Notes

---
+++ Hashes/Notes.rb
@@ -0,0 +1,12 @@
+#old syntax
+old_syntax_hash = {:name => 'bob'}
+
+#new syntax
+new_hash = {name: 'bob'}
+
+person = { height: '6 ft', weight: '160 lbs'}
+
+#add to hash 
+person[:hair] = 'brown'
+
+#remove from hash
