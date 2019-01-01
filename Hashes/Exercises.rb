b89f14eb80b7a06625391f0bd726f5cb1c6dd86b
Jose <joegonzo990@gmail.com>
2019-01-01 20:21:15 +0000

    Hashes Final:

--- Hashes/Exercises.rb
+++ Hashes/Exercises.rb
@@ -56,5 +56,32 @@ puts "############## 5 ###############"
 #  puts "Nope!"
 #end
 
-puts "############## 5 ###############"
+puts "############## 6 ###############"
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+result = {}
 
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+puts "############## 7 ###############"
+x = "hi there"
+my_hash = {x: "some value"}
+# In my_hash x is the symbol x
+my_hash2 = {x => "some value"}
+# In my_hash2 x is the variable
+
+puts "############## 8 ###############"
+#NoMethodError: undefined method `keys' for Array
+#No method called keys for arrays
